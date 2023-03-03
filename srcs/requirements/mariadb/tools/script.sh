7e2e651a34a251f9fa16e46b7f12e45084e6bdcc
yoonkiung <rldnd4429@gmail.com>
2023-03-03 15:48:04 +0900

    드디어 성공

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,32 +3,18 @@
 set -e
 set -x
 
-echo "in script"
+service mysql start;
 
-service mysql start
 #create user
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "show databases";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "create user if not exists ${MYSQL_USER}@'%' identified by '${MYSQL_PASSWORD}'";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@'%'";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "flush privileges";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "show databases";
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}";
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'";
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%'";
+mysql -e "FLUSH PRIVILEGES";
 
 
 # # change root name and password
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "set password for 'root'@'localhost' = password('${MYSQL_ROOT_PASSWORD}')";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "flush privileges";
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES";
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES";
 
-mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD proc
+mysql $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
 mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
-echo "\ndaemon off;" >> /etc/mysql/mariadb.cnf
-# sleep 100
-# exec mysqld_safe
-# service mysql start
-# mysqladmin -u root password "$MYSQL_ROOT_PASSWORD" 
-# mysql -u root -p"$DATABASE_ROOT_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_ROOT_PASS') WHERE User='root'" 
-# mysql -u root -p"$DATABASE_ROOT_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')" 
-# mysql -u root -p"$DATABASE_ROOT_PASS" -e "DELETE FROM mysql.user WHERE User=''" 
-# mysql -u root -p"$DATABASE_ROOT_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'" 
-# mysql -u root -p"$DATABASE_ROOT_PASS" -e "FLUSH PRIVILEGES"
+exec mysqld_safe
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+chown -R www-data:www-data /var/www/
+chown -R www-data:www-data /var/www/html
+cd /var/www/html
+
+
+sudo -u	www-data sh -c "wp core download --locale=$WORDPRESS_LOCAL"
+sudo -u	www-data sh -c "wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_NAME"
+sudo -u	www-data sh -c "wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email"
+sudo -u	www-data sh -c "wp user create $WORDPRESS_USER_NAME $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD"
+sudo -u	www-data sh -c "wp plugin update --all"
+
+php-fpm7.3 --nodaemonize
