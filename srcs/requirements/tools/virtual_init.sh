e8f96a1ce91cbc696e5642c88132c2e83a705ac6
Kiung Yoon <kiyoon@c3r6s4.42seoul.kr>
2023-03-08 15:07:43 +0900

    virtual버전

--- srcs/requirements/tools/virtual_init.sh
+++ srcs/requirements/tools/virtual_init.sh
@@ -2,12 +2,17 @@
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt install vim -y
 sudo apt install openssh-server -y
-sudo apt install git -y
+sudo apt install openssh-client -y
+
 sudo apt install ufw -y
 sudo ufw enable
 sudo ufw allow 4242/tcp
-curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo apt install vim -y
+sudo apt install git -y
+sudo apt install curl -y
+
+curl -4fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo apt-get install docker-compose
